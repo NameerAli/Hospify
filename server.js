7f28b07958202257906628289ad483dae542efd1
Syed Nameer Ali <50754699+NameerAli@users.noreply.github.com>
2023-05-01 21:42:14 +0500

    final commit

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 require('dotenv').config();
 const dbConfig = require("./config/dbConfig.js");
 const userRoute = require('./routes/user_routes');
-
+const adminRoute = require('./routes/admin_routes');
+const doctorRoute = require('./routes/doctor_routes');
 
 
 const port = process.env.PORT || 5000;
@@ -11,3 +12,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Node Server started at port',port));
 app.use(express.json());
 app.use('/api/user',userRoute);
+app.use('/api/admin',adminRoute);
+app.use('/api/doctor',doctorRoute);
